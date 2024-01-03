fa68bdb7bdb0e3ece6d53e00e1e2b4beaec14c24
GDB <gdb.1795@gmail.com>
2024-01-03 16:11:32 +0530

    This is java code

---
+++ first.java
@@ -0,0 +1,30 @@
+import.java.util.com
+
+public class calculator{
+
+public void setterAdd(int a,int b)
+{
+c=a+b;
+System.out.println("This is add function"+c);
+
+}
+
+
+public static void main(String[] args)
+{
+
+calculator c=new calculator();
+c.setterAdd(2,4);
+
+
+
+
+
+}
+
+
+
+
+
+}
+
